772c9b6fdd4e5cc4659819ebb5313b7512cebf7b
marcioviegas <marcioaugustoviegas@gmail.com>
2020-01-10 00:58:43 -0300

    redudant-closures.js

---
+++ redundant-closures.js
@@ -0,0 +1,11 @@
+function redundant(str) {
+    return function() {
+        return str;
+    }
+}
+
+const f1 = redundant("apple");
+console.log(f1());
+
+const f2 = redundant("pear");
+console.log(f2());
