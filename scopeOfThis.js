0213e0c02a477d9cbe579c7ee7ec24f53ea49f48
Márcio Viegas <marcioaugustoviegas@gmail.com>
2020-01-27 10:28:18 -0300

    Create scopeOfThis to prove I am not crazy
    A function: [object global]
    Anonymous function inside a function on a object: [object global]
    Anonymous function inside a function on a object with this passed as argument: [object Object]
    Function inside a object: [object Object]

---
+++ scopeOfThis.js
@@ -0,0 +1,25 @@
+function test() {
+  console.log("A function: " + this);
+}
+
+var test2 = {
+  example: function() {
+    (function() {
+      console.log("Anonymous function inside a function on a object: " + this);
+    })();
+
+    (function(scope) {
+      this.self = scope;
+      console.log(
+        "Anonymous function inside a function on a object with this passed as argument: " +
+          self
+      );
+    })(this);
+
+    console.log("Function inside a object: " + this);
+  }
+};
+
+test();
+
+test2.example();
